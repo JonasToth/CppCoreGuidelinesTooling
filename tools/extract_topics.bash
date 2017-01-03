b9d84c1f8b1e34bf81f482d3ab340f91feecc32e
Jonas Toth <jonas.toth@gmail.com>
2017-01-03 22:19:19 +0100

    [Feature] Extracting topic shortcuts

--- tools/extract_topics.bash
+++ tools/extract_topics.bash
@@ -12,4 +12,11 @@ if [ $# -ne 1 ]; then
     exit -1
 fi
 
-egrep -hn -e '\# <a name="(.+)"></a>(\w+): (\w+)' $1 | sed -ne '1,22p'
+#egrep -hn -e '\# <a name="(.+)"></a>(\w+): (\w+)' $1 | sed -ne '1,22p'
+# extract all links (see extract_linksummary.bash), get the shortcut for it
+# sort it, make uniq
+# this means only one line is left for each topic
+topics=$(sed -nre "s/\* \[(\w+)\.(.*)\].*/\1/pg" $1 | sort | uniq)
+
+# remove known false results and trim whitespace (R has)
+echo "$topics" | sed -e '/Geosoft/d' -e '/isocpp/d' -e '/Possibility/d' | sed -e 's/^[[:space:]]*//'
