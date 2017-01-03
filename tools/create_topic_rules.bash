524e560f99d679a692be5b6b283c6b9e6fa0b1f2
Jonas Toth <jonas.toth@gmail.com>
2017-01-04 00:02:17 +0100

    [Feature] Create rule files in guideline

--- tools/create_topic_rules.bash
+++ tools/create_topic_rules.bash
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 Guidelines TopicShortcut"
+    echo
+    echo "Provide correct number of arguments"
+    exit -1
+fi
 
+# get the correct script name no matter from where the script got called
+number_script=$(echo "$0" | sed -e "s/create_topic_rules/extract_rule_numbers/g")
+heading_script=$(echo "$0" | sed -e "s/create_topic_rules/extract_rules/g")
+
+if [ ! -d $2 ]; then
+    echo "Topic directory does not exist!" 2>&1
+    exit -1
+else
+    cd "$2"
+    rule_headings=$(eval "../$heading_script" "../$1" "$2")
+    rule_numbers=$(eval "../$number_script" "../$1" "$2")
+
+    # write the heading in a file name rule_nbr
+    while read rule_nbr
+    do
+        echo "$rule_nbr"
+        echo "$rule_headings" | sed -ne "/$2\.${rule_nbr}:/p" > ${rule_nbr}
+    done <<< "$rule_numbers"
+    cd ..
+fi
