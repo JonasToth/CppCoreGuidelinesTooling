14781df7e94e0cc90047777fe088b8912157ac36
Jonas Toth <jonas.toth@gmail.com>
2017-01-03 20:08:14 +0100

    [Misc] Download script improved

--- tools/get_guidelines.bash
+++ tools/get_guidelines.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # download the guidelines via wget and safe them as CppCoreGuidelines.md
-wget https://raw.githubusercontent.com/isocpp/CppCoreGuidelines/master/CppCoreGuidelines.md
+wget https://raw.githubusercontent.com/isocpp/CppCoreGuidelines/master/CppCoreGuidelines.md -O CppCoreGuidelines.md
