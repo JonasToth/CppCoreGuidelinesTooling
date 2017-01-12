37a41ba6e464dbfedd99070bf1026d1d786e5c46
Jonas Toth <jonas.toth@gmail.com>
2017-01-12 22:38:46 +0100

    [Feature] start merging scripts

---
+++ tools/merge_rule.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# merge a rule enforcement with the tool comment
+# e.g. write how clang implements the enforcement in another file, merging these two
+# files
---
+++ tools/merge_topic.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# merge all merged rules to a big topic file.
+# these topics will then be merged into a big CppTooling-File
