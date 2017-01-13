a7b4d2052e52d9926c93d62551a593a74524c57a
Jonas Toth <jonas.toth@gmail.com>
2017-01-13 20:07:02 +0100

    [Misc] improve tool file generation

--- tools/add_rule_tool.bash
+++ tools/add_rule_tool.bash
@@ -9,19 +9,20 @@ topic_script=$(echo $0 | sed -e 's/add_rule_tool/extract_topics/g')
 eval "$topic_script CppCoreGuidelines.md" |
 while read topic
 do
-    if [ ! -d "$topic" ]; then
-        cd "$topic"
+    echo "Working on $topic"
+    cd "$topic"
 
-        # for each rule file, that has more then 2 lines, touch a file in the tool
-        ls -1 | sed -e '/links.md/d' |
-        while read rule
-        do
-            # check for line number
-            if [[ $(wc -l "$rule" | sed -ne 's/\(.*\) .*/\1/pg') -gt 2 ]]; then
-                touch "../../clang_tidy/$rule"
-            fi
-        done
+    # for each rule file, that has more then 2 lines, touch a file in the tool
+    $(ls -1 | sed -e '/links.md/d') |
+    while read rule
+    do
+        # check for line number
+        if [[ $(wc -l "$rule" | sed -ne 's/\(.*\) .*/\1/pg') -gt 2 ]]; then
+            touch "../../clang_tidy/$topic/$rule"
 
-        cd ..
-    fi
+            echo "clang-tidy $rule"
+        fi
+    done
+
+    cd ..
 done
