416c66797163d38d06300d15fc84079955c3108e
Jonas Toth <jonas.toth@gmail.com>
2017-01-14 14:05:50 +0100

    [Misc] cronjob script created

---
+++ tools/update_everything.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# the whole process to update the guidelines, get all rules, topics and so on
+# and create the files for them.
+# this shall be used as a cronjob
+#
+# call from guideline directory
+
+
+../tools/create_topic_directories.bash 
+../tools/create_topic_summaries.bash 
+../tools/create_topic_rules.bash CppCoreGuidelines.md
+
+
