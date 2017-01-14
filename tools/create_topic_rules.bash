c4ed10ffc9945d9b77cc971c7b41213aa5e404f9
Jonas Toth <jonas.toth@gmail.com>
2017-01-14 14:02:07 +0100

    [Misc] update the script to work with all topics, not a topic as argument

--- tools/create_topic_rules.bash
+++ tools/create_topic_rules.bash
@@ -13,39 +13,34 @@ heading_script=$(echo "$0" | sed -e "s/create_topic_rules/extract_rules/g")
 enforcement_script=$(echo "$0" | sed -e "s/create_topic_rules/extract_enforcements/g")
 topic_script=$(echo $0 | sed -e 's/create_topic_rules/extract_topics/g')
 
-if [ ! -d $2 ]; then
-    echo "Topic directory does not exist!" 2>&1
-    exit -1
-else
-    eval "$topic_script CppCoreGuidelines.md" |
-    while read topic
+eval "$topic_script CppCoreGuidelines.md" |
+while read topic
+do
+    cd "$topic"
+    echo "$topic"
+    rule_headings=$(eval "../$heading_script" "../$1" "$topic")
+    rule_numbers=$(eval "../$number_script" "../$1" "$topic")
+
+    # write the heading in a file name rule_nbr
+    while read rule_nbr
     do
-        cd "$topic"
-        echo "$topic"
-        rule_headings=$(eval "../$heading_script" "../$1" "$topic")
-        rule_numbers=$(eval "../$number_script" "../$1" "$topic")
+        # find the relevant rule for this number and topic
+        rule_line=$(echo "$rule_headings" | sed -ne "/$topic\.${rule_nbr}:/p") 
 
-        # write the heading in a file name rule_nbr
-        while read rule_nbr
-        do
-            # find the relevant rule for this number and topic
-            rule_line=$(echo "$rule_headings" | sed -ne "/$topic\.${rule_nbr}:/p") 
+        # extract the linenbr to search the enforcement afterwards
+        rule_occurence_line=$(echo "$rule_line" | sed -e "s/\(:#.*\)//g")
+        rule_line=$(echo "$rule_line" | sed -e "s/$rule_occurence_line://g")
 
-            # extract the linenbr to search the enforcement afterwards
-            rule_occurence_line=$(echo "$rule_line" | sed -e "s/\(:#.*\)//g")
-            rule_line=$(echo "$rule_line" | sed -e "s/$rule_occurence_line://g")
+        echo "$rule_line"
+        echo $rule_occurence_line
 
-            echo "$rule_line"
-            echo $rule_occurence_line
+        # output the heading, removing the linenbr from the rule_line
+        echo "$rule_line" > ${rule_nbr}
+        echo "" >> ${rule_nbr} 
+        
+        # insert enforcement
+        eval "../$enforcement_script ../$1 $rule_occurence_line" >> ${rule_nbr}
+    done <<< "$rule_numbers"
 
-            # output the heading, removing the linenbr from the rule_line
-            echo "$rule_line" > ${rule_nbr}
-            echo "" >> ${rule_nbr} 
-            
-            # insert enforcement
-            eval "../$enforcement_script ../$1 $rule_occurence_line" >> ${rule_nbr}
-        done <<< "$rule_numbers"
-
-        cd ..
-    done
-fi
+    cd ..
+done
