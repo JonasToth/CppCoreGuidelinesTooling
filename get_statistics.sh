5c75d36015c497df86558315e14bacbe592b30c2
Jonas Toth <jonas.toth@gmail.com>
2017-09-25 12:45:05 +0200

    statistics per sections and total + percentage

--- get_statistics.sh
+++ get_statistics.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
+enforcement_statistics() {
+    # count the number of big headings, these are enforceable rules
+    RULES=$(cat "$1" | grep '#' | wc -l)
+    # count the number of 'no enforcement' comments in the document
+    NO_ENFORCEMENT=$(cat "$1" | grep 'no enforcement' | wc -l)
+    # calculate the difference between enforced and not enforced
+    ENFORCED=$(($RULES-$NO_ENFORCEMENT))
 
-RULES=$(cat *.md | grep '#' | wc -l)
-NO_ENFORCEMENT=$(cat *.md | grep 'no enforcement' | wc -l)
-ENFORCED=$(($RULES-$NO_ENFORCEMENT))
+    PERCENTAGE=$(echo "scale=2; 100. * $ENFORCED / $RULES" | bc)
+    echo "enforceable/done/todo ($RULES/$ENFORCED/$NO_ENFORCEMENT) $PERCENTAGE% done"
+    echo
+}
 
-echo "number of rules: \t\t\t$RULES"
-echo "number of enforced rules:\t\t$ENFORCED"
-echo "number of rules without enforcement:\t$NO_ENFORCEMENT"
+# create a temporary md file, with all sections contained. Used for the total statistics.
+cat *.md > temp_all.md
+
+echo "=== Total statistics"
+enforcement_statistics "temp_all.md"
+
+# remove the temporary file to not polute everything
+rm temp_all.md
+
+for file in [a-z_]*.md
+do
+    echo "== $file statistics"
+    enforcement_statistics $file
+done
